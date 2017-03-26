e783931768bc9d84059d707b0143fade08f73cea
Mark Hobro <mhobro@kth.se>
2017-03-26 14:23:13 +0200

    Fixed for example full screen on exercise 2 for lab 1

--- js/lab1.js
+++ js/lab1.js
@@ -64,5 +64,17 @@ var map;
         map.setCenter(myLoc);
       }
 
+      function toggleFullScreen() {
+        var doc = window.document;
+        var docEl = doc.documentElement;
 
+        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
 
+        if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+          requestFullScreen.call(docEl);
+        }
+        else {
+          cancelFullScreen.call(doc);
+        }
+      }
